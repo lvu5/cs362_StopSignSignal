97fa06984f5586612f6c2c13011e33b7b96c1a90
lvu5 <lvu5@uic.edu>
2023-04-25 14:55:17 -0500

    milestone 8

--- sub1_dataCollector.ino
+++ sub1_dataCollector.ino
@@ -1,109 +1,141 @@
-#include <QueueList.h>
+#include <SoftwareSerial.h>
+#include <NewPing.h>
+// #include <Tone.h>
 
-// Using NESW ask a direction of sensor
-const int N_trigPin = 2; // Trigger Pin of Ultrasonic Sensor
-const int N_echoPin = 3; // Echo Pin of Ultrasonic Sensor
+// Define the transmission pins and baud rate
+#define NUM_SONAR 4
+const int RX_PIN = A0;
+const int TX_PIN = A1;
+const int BAUD_RATE = 9600;
+const int threshold = 1; // in centimeters
 
-const int E_trigPin = 4; 
-const int E_echoPin = 5; 
+// SoftwareSerial receiverSerial(RX_PIN, TX_PIN);
 
-const int S_trigPin = 6; 
-const int S_echoPin = 7; 
 
-const int W_trigPin = 8; 
-const int W_echoPin = 9; 
 
-// Initialize a queue to store the sensor readings
-QueueList<String> readingsQueue;
+const int buzzer_pin = 10;
+unsigned long previousMillis = 0;
+const long interval = 500;
 
-// Calculate distance between object and sensor
-float getDistance(int trigPin, int echoPin) {
-  // Send a trigger pulse to start the measurement
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  
-  // Measure the duration of the echo pulse
-  long duration = pulseIn(echoPin, HIGH);
-  
-  // Calculate the distance using the speed of sound (343 meters per second)
-  float distance = duration / 2.0 * 0.0343; // distance in meters
-  
-  return distance;
-}
+const int N_trigPin = 2;
+const int N_echoPin = 3;
 
-// Calculating the distance to the object
-// Returns the time it takes for the ultrasonic 
-// waves to travel from the sensor to the object and back
-long getTimeOfArrival(int trigPin, int echoPin) {
-  // Send a trigger pulse to start the measurement
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  
-  // Measure the duration of the echo pulse
-  long duration = pulseIn(echoPin, HIGH);
-  
-  return duration;
-}
+const int E_trigPin = 4;
+const int E_echoPin = 5;
+
+const int S_trigPin = 6;
+const int S_echoPin = 7;
+
+const int W_trigPin = 8;
+const int W_echoPin = 9;
+
+const int north = 0;
+const int west = 1;
+const int south = 2;
+const int east = 3;
+
+const char direction_arr[4] = {'n','w','s','e'};
+
+NewPing sonar[NUM_SONAR] = {
+    NewPing(N_trigPin, N_echoPin, 30),
+    NewPing(W_trigPin, W_echoPin, 30),
+    NewPing(S_trigPin, S_echoPin, 30),
+    NewPing(E_trigPin, E_echoPin, 30)
+};
+
+unsigned int dist_arr[NUM_SONAR] = {0};
+unsigned int dist_arr_prev[NUM_SONAR] = {0};
+
+const int N_buzzer = 10;
+const int E_buzzer = 11;
+const int S_buzzer = 12;
+const int W_buzzer = 13;
+
+int dist_n = INT32_MAX;
+int dist_e = INT32_MAX;
+int dist_s = INT32_MAX;
+int dist_w = INT32_MAX;
+int distance = 0;
+
+String SerialData = "";
+char data;
+char curr;
+char prev = 'a';
+
+// void playTone(int duration) {
+//   digitalWrite(buzzer_pin, HIGH);
+//   delay(duration);
+//   digitalWrite(buzzer_pin, LOW);
+//   delay(duration);
+// }
 
 void setup() {
-   Serial.begin(9600); // Starting Serial Terminal
+    Serial.begin(BAUD_RATE);
+    pinMode(buzzer_pin, OUTPUT);
+    // receiverSerial.begin(BAUD_RATE);
+    // for(int i = 2; i < 10; i++){
+    //     pinMode(i, OUTPUT);
+    // }
+}
+
+unsigned int calc_distance(NewPing sensor) {
+    return sensor.ping() / US_ROUNDTRIP_CM;
+}
+
+int min_distance(){
+    // take the index
+    int min = INT16_MAX;
+    int min_index = -1;
+    for (int i = 0; i < NUM_SONAR; i++) {
+        if (min > dist_arr[i] && dist_arr[i] > 0){
+            min_index = i;
+            min = dist_arr[i];
+        }
+    }
+    return min_index;
 }
 
 void loop() {
-  // Measure the time of arrival for each sensor
-  long N_duration = getTimeOfArrival(N_trigPin, N_echoPin);
-  long E_duration = getTimeOfArrival(E_trigPin, E_echoPin);
-  long S_duration = getTimeOfArrival(S_trigPin, S_echoPin);
-  long W_duration = getTimeOfArrival(W_trigPin, W_echoPin);
+    delay(50);
+    unsigned long currentMillis = millis();
+    for(int i = 0; i < NUM_SONAR; i++){
+        dist_arr[i] = calc_distance(sonar[i]);
+        if (dist_arr[i] == 0)
+            dist_arr_prev[i] = 0;
+    }
+    int min_index = min_distance();     
 
-  // Determine which sensor detected the object first
-  String firstSensor = "";
-  long firstDuration = 0;
+    // Serial.println(min_index);
+    // Serial.println(dist_arr[2]);
+    // Serial.print("The shortest object detect from ");
+    // Serial.println(direction_arr[min_index]);
+    // Serial.println(min_index);
+    // Serial.println(dist_arr[min_index]);
 
-  if (N_duration < E_duration && N_duration < S_duration && N_duration < W_duration) {
-    firstSensor = "North";
-    firstDuration = N_duration;
-  } else if (E_duration < S_duration && E_duration < W_duration) {
-    firstSensor = "East";
-    firstDuration = E_duration;
-  } else if (S_duration < W_duration) {
-    firstSensor = "South";
-    firstDuration = S_duration;
-  } else {
-    firstSensor = "West";
-    firstDuration = W_duration;
-  }
 
-  // Add the result to the readings queue
-  String reading = firstSensor + "," + String(firstDuration);
-  readingsQueue.push(reading);
+    // Serial.println(calc_distance(sonar[0]));
 
-  // Print the readings queue
-  Serial.println("Readings queue:");
-  for (int i = 0; i < readingsQueue.size(); i++) {
-    Serial.println(readingsQueue.get(i));
-  }
 
-//   // Send the queue of directions over serial
-//   while (!directionsQueue.isEmpty()) {
-//     String direction = directionsQueue.pop();
-//     Serial.println(direction);
-//   }
-
-  // Wait for a brief period before taking the next reading
-  delay(100);
+    if (dist_arr[min_index] < 5 && dist_arr[min_index] > 0) {
+        // Serial.println("Hello sent");
+        // object detected
+        
+        sendData(direction_arr[min_index], min_index);
+        
+        // playTone(100);
+        if (currentMillis - previousMillis >= interval) {
+            previousMillis = currentMillis;
+            tone(buzzer_pin, 1000);
+            delay(500);
+            noTone(buzzer_pin);
+        }
+    }
 }
 
-//long microsecondsToInches(long microseconds) {
-   //return microseconds / 74 / 2;
-//}
-//
-//long microsecondsToCentimeters(long microseconds) {
-   //return microseconds / 29 / 2;
-//}
+void sendData(char direction, int min_index) {
+    if (direction != prev || abs(dist_arr_prev[min_index] - dist_arr[min_index]) > threshold) {
+            dist_arr_prev[min_index] = dist_arr[min_index];
+            prev = direction;
+            Serial.write(direction);
+    }
+}
--- sub2_controller.ino
+++ sub2_controller.ino
@@ -1,54 +1,128 @@
-#include <Queue.h>
+#include <IRremote.h>
+#include <SoftwareSerial.h>
+#include <TimerOne.h>
 
-Queue<String> directionsQueue; // Declare a queue of strings
+// Define the transmission pins and baud rate
+const int RX_PIN = A0;
+const int TX_PIN = A1;
+const int BAUD_RATE = 9600;
+// const int IR_PIN = 2;  // IR receiver pin
 
-const int N_ledPin = 2;
-const int E_ledPin = 3;
-const int S_ledPin = 4;
-const int W_ledPin = 5;
+// SoftwareSerial remoteSerial(RX_PIN, TX_PIN);
+// IRrecv irrecv(IR_PIN);
+// decode_results results;
 
-void setup()
-{
-  pinMode(N_ledPin, OUTPUT);
-  pinMode(E_ledPin, OUTPUT);
-  pinMode(S_ledPin, OUTPUT);
-  pinMode(W_ledPin, OUTPUT);
-  
-  // Turn on all LEDs initially
-  digitalWrite(N_ledPin, HIGH);
-  digitalWrite(E_ledPin, HIGH);
-  digitalWrite(S_ledPin, HIGH);
-  digitalWrite(W_ledPin, HIGH);
+// Remote
+const int RECV_PIN = 10;
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+bool isOn = false;
+//
 
-  // Set up serial communication
-  Serial.begin(9600);
+const int N_red = 2;
+const int E_red = 4;
+const int S_red = 6;
+const int W_red = 8;
+
+void setup() {
+    pinMode(N_red, OUTPUT);
+    pinMode(E_red, OUTPUT);
+    pinMode(S_red, OUTPUT);
+    pinMode(W_red, OUTPUT);
+
+    // digitalWrite(N_red, HIGH);
+    // digitalWrite(E_red, HIGH);
+    // digitalWrite(S_red, HIGH);
+    // digitalWrite(W_red, HIGH);
+
+    digitalWrite(N_red, LOW);
+    digitalWrite(E_red, LOW);
+    digitalWrite(S_red, LOW);
+    digitalWrite(W_red, LOW);
+
+    Serial.begin(BAUD_RATE);
+    // remoteSerial.begin(BAUD_RATE);
+
+    irrecv.enableIRIn();  // Start the IR receiver
+    irrecv.blink13(true);
+
+    // Timer1.initialize(1000000);  // initialize timer1, and set a 1/2 second period
+    Timer1.attachInterrupt(buttonClick);  // attaches callback() as a timer overflow interrupt
 }
 
-void loop()
-{
-  // Check if there are any directions in the serial buffer
-  while (Serial.available()) {
-    // Read the next direction from the serial buffer
-    String direction = Serial.readStringUntil('\n');
-
-    // Add the direction to the queue
-    directionsQueue.push(direction);
-  }
-
-  // Process the next direction in the queue
-  if (!directionsQueue.isEmpty()) {
-    // Get the first direction in the queue
-    String direction = directionsQueue.pop();
-
-    // Turn off the corresponding LED based on the direction
-    if (direction == "North") {
-      digitalWrite(N_ledPin, LOW);
-    } else if (direction == "East") {
-      digitalWrite(E_ledPin, LOW);
-    } else if (direction == "South") {
-      digitalWrite(S_ledPin, LOW);
-    } else if (direction == "West") {
-      digitalWrite(W_ledPin, LOW);
+void loop() {
+    if (Serial.available() > 0) {
+        char received = Serial.read();
+        controlRedLights(received);
+        Serial.println(received);
     }
-  }
+    if (isOn) {
+        blinkAllRedLights();
+    } else {
+        digitalWrite(N_red, LOW);
+        digitalWrite(E_red, LOW);
+        digitalWrite(S_red, LOW);
+        digitalWrite(W_red, LOW);
+    }
+}
+
+void controlRedLights(char direction) {
+    int redPin = 0;
+
+    switch (direction) {
+        case 'n':
+            redPin = N_red;
+            break;
+        case 'e':
+            redPin = E_red;
+            break;
+        case 's':
+            redPin = S_red;
+            break;
+        case 'w':
+            redPin = W_red;
+            break;
+    }
+
+    // if (direction == 'n')
+    //     redPin = N_red;
+    // if (direction == 'e')
+    //     redPin = E_red;
+    // if (direction == 's')
+    //     redPin = S_red;
+    // if (direction == 'w')
+    //     redPin = W_red;
+
+    if (redPin != 0) {
+        digitalWrite(redPin, HIGH);
+        delay(1000);  // Duration for which the light remains off
+        digitalWrite(redPin, LOW);
+    }
+    redPin = 0;
+}
+
+void buttonClick() {
+    if (irrecv.decode(&results)) {
+        // Serial.println(results.value, HEX);
+        delay(1000);
+        isOn = !isOn;
+        // if (results.value == 0xFF30CF) {  // Change this value to the remote control "1" button code
+        blinkAllRedLights();
+        // }
+        irrecv.resume();  // Receive the next value
+    }
+}
+
+void blinkAllRedLights() {
+    // blink 4 LEDs
+    digitalWrite(N_red, LOW);
+    digitalWrite(E_red, LOW);
+    digitalWrite(S_red, LOW);
+    digitalWrite(W_red, LOW);
+    delay(200);
+    digitalWrite(N_red, HIGH);
+    digitalWrite(E_red, HIGH);
+    digitalWrite(S_red, HIGH);
+    digitalWrite(W_red, HIGH);
+    delay(200);
 }
